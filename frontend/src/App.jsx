1927cd5d86b7469ed65c3890884bc82a47a26559
YURA <dbfk0484@hnmaail.net>
2022-03-14 10:47:54 +0900

    두번째

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import Home from "./pages/Home";
+import RouterComponent from "./route/RouterComponent";
+
+const App = () => {
+    return (
+
+
+        <RouterComponent>
+            <Home />
+        </RouterComponent>
+
+    );
+
+
+
+};
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Intro from '../components/Intro';
+import Navbar from '../Navbar/Navbar';
+import Cart from '../components/Cart'
+import Coupon from "../components/Coupon";
+import Main from "../components/Main";
+import Delivery from "../components/Delivery";
+import Mileage from "../components/Mileage";
+import Order from "../components/Order";
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Main />
+            <Coupon />
+            <Delivery />
+            <Mileage />
+            <Order />
+
+        </div>
+    );
+};
+
+export default Home;
