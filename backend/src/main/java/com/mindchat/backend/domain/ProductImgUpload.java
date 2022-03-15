b62531383091530b677b53b431dc79679b0388e3
YURA <dbfk0484@hnmaail.net>
2022-03-15 17:40:38 +0900

    YURA

--- backend/src/main/java/com/mindchat/backend/domain/ProductImgUpload.java
+++ backend/src/main/java/com/mindchat/backend/domain/ProductImgUpload.java
@@ -2,9 +2,9 @@ package com.mindchat.backend.domain;
 
 public class ProductImgUpload {
 
-    private int img_seq;
-    private int p_seq;
-    private String img_filename1;
+    private int img_seq; // 이미지 번호
+    private int p_seq; // 상품 번호
+    private String img_filename1; // 파일이름
     private String img_filename2;
     private String img_filename3;
     private String img_filename4;
--- backend/src/main/java/com/mindchat/backend/mapper/ProductImgMapper.java
+++ backend/src/main/java/com/mindchat/backend/mapper/ProductImgMapper.java
@@ -1,8 +1,15 @@
 package com.mindchat.backend.mapper;
 
-import org.springframework.web.bind.annotation.PostMapping;
 
+import com.mindchat.backend.domain.ProductImgUpload;
+import com.mindchat.backend.domain.ProductInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public class ProductImgMapper {
+
+    List<ProductImgUpload> getProductList(); // 상품리스트 목록
+    ProductInfo Product
 
 
 
